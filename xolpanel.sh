20986e7dfaead1f9b13e66a02cfa1f07a5db0f9e
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-08-03 12:50:21 +0700

    Update xolpanel.sh

--- xolpanel.sh
+++ xolpanel.sh
@@ -5,8 +5,8 @@ rm -rf xolpanel.sh
 apt update && apt upgrade
 apt install python3 python3-pip git
 git clone https://github.com/fians-xd/xolpanel.git
-unzip xolpanel/xolpanel.zip
-pip3 install -r xolpanel/requirements.txt
+unzip panel_vps_conf/xolpanel.zip
+pip3 install -r panel_vps_conf/requirements.txt
 pip3 install pillow
 
 #isi data
@@ -14,10 +14,10 @@ echo ""
 read -e -p "[*] Input your Bot Token : " bottoken
 read -e -p "[*] Input Your Id Telegram :" admin
 read -e -p "[*] Input Your Subdomain :" domain
-echo -e BOT_TOKEN='"'$bottoken'"' >> /root/xolpanel/var.txt
-echo -e ADMIN='"'$admin'"' >> /root/xolpanel/var.txt
-echo -e DOMAIN='"'$domain'"' >> /root/xolpanel/var.txt
-clear
+echo -e BOT_TOKEN='"'$bottoken'"' >> /root/panel_vps_conf/var.txt
+echo -e ADMIN='"'$admin'"' >> /root/panel_vps_conf/var.txt
+echo -e DOMAIN='"'$domain'"' >> /root/panel_vps_conf/var.txt
+#clear
 echo "Done"
 echo "Your Data Bot"
 echo -e "==============================="
@@ -45,13 +45,13 @@ END
 systemctl start xolpanel 
 systemctl enable xolpanel
 
-clear
+#clear
 
 echo -e "==============================================="
 echo " Installations complete, type /menu on your bot"
 echo -e "==============================================="
 read -n 1 -s -r -p "Press any key to Reboot"
 rm -rf xolpanel.sh
-clear
-reboot
+#clear
+#reboot
 
