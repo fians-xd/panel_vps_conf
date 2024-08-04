75b0a3f48804f38d881d11d52c9fdee840930629
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-08-04 14:40:40 +0700

    Update xolpanel.sh

--- xolpanel.sh
+++ xolpanel.sh
@@ -6,7 +6,6 @@ apt update && apt upgrade
 apt install python3 python3-pip git
 git clone https://github.com/fians-xd/panel_vps_conf.git
 unzip panel_vps_conf/xolpanel.zip
-cd
 pip3 install -r xolpanel/requirements.txt
 pip3 install pillow
 
