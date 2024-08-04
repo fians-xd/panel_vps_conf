37c1cd6e14c9633ff305e29dc929231ba4c70ad3
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-08-05 06:59:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 rm install.sh
 apt update
+sudo apt-get install libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk libharfbuzz-dev libfribidi-dev libxcb1-dev
 pip3 cache purge
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
