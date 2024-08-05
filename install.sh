fbf7b24ae1e9e12bed071e814a56c11be011bd94
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-08-05 13:46:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rm install.sh
 apt update
 apt install python3-pip
 python3 -m pip install --upgrade pip
-pip install --upgrade idna
+pip3 install --upgrade idna
 apt install python3-pil
 git clone https://github.com/fians-xd/panel_vps_conf.git
 apt install libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk libharfbuzz-dev libfribidi-dev libxcb1-dev
