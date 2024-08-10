c324a56b7084315cd161876c893b9d9ae9f476b0
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-08-10 13:02:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ pip3 install --upgrade idna
 apt install python3-pil -y
 git clone https://github.com/fians-xd/panel_vps_conf.git
 apt install libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk libharfbuzz-dev libfribidi-dev libxcb1-dev -y
-pip3 install python-telegram-bot setuptools wheel telegram
+pip3 install aiogram python-telegram-bot setuptools wheel telegram
 pip3 cache purge
 
 # Collect user inputs
