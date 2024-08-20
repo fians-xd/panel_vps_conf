4a68ceda0fdd87871e8c596fb993b82795021d29
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-08-20 14:02:52 +0700

    Update trialvmess.py

--- trialvmess.py
+++ trialvmess.py
@@ -20,7 +20,7 @@ def create_vmess_account():
         try:
             with open('/etc/log-create-vmess-trial-clean.log', 'r') as file:
                 log_content = file.read()
-            return f'Vmess Account created:\n\n{log_content}'
+            return f'{log_content}'
         except FileNotFoundError:
             logger.error('Log file not found.')
             return 'Log file not found.'
