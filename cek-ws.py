0c17bcd8a8748be466247dfa6d54ddd7dff0725a
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-08-20 14:45:24 +0700

    Update cek-ws.py

--- cek-ws.py
+++ cek-ws.py
@@ -20,7 +20,7 @@ def cek_vmess():
         try:
             with open('/etc/cek-vmess.log', 'r') as file:
                 log_content = file.read()
-            return f'{log_content}'
+            return f'User Login Vmess:-\n{log_content}'
         except FileNotFoundError:
             logger.error('Log file not found.')
             return 'Log file not found.'
