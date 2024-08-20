14a849c7322b051dd3fe631fb59f07eae9bfe91b
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-08-20 13:58:28 +0700

    Update cek-vless.py

--- cek-vless.py
+++ cek-vless.py
@@ -20,7 +20,7 @@ def cek_vless():
         try:
             with open('/etc/cek-vless.log', 'r') as file:
                 log_content = file.read()
-            return f'User Login Vless Account:\n{log_content}'
+            return f'{log_content}'
         except FileNotFoundError:
             logger.error('Log file not found.')
             return 'Log file not found.'
