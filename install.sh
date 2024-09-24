4e9c409eb43076c9e501f19c8d2658c7523e88b1
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-09-24 21:59:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,10 @@ mkdir /mnt/.config
 
 cd /mnt/.obscure/.data/.complex/.path/.secret/.layer/.cryptic/.depth/.structure/.area/.panel_vps_conf
 
+echo " "
 echo -e "${biru}~=[ ${green}Please Wait, the System is Being Configured..! ${biru}]=~${NC}"
+echo " "
+echo " "
 
 # Get tools
 wget -q https://raw.githubusercontent.com/fians-xd/panel_vps_conf/master/runbot.py
